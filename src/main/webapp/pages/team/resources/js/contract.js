82b1f2e2f4567c29c56920f4e53ef42151b325e8
Torma Péter <ptorma2011@gmail.com>
2017-03-08 20:54:42 +0100

    ez game ez life

--- src/main/webapp/pages/team/resources/js/contract.js
+++ src/main/webapp/pages/team/resources/js/contract.js
@@ -1,3 +1,25 @@
+$(document).ready(function() {
+    $("#div_amount").keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+             // Allow: Ctrl/cmd+A
+            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+             // Allow: Ctrl/cmd+C
+            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+             // Allow: Ctrl/cmd+X
+            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+             // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+});
+
 var showMoadal = function() {
 	$('#contractModal').modal('show');
 }
@@ -21,6 +43,8 @@ function validateContract() {
 			message : "Wrong email format."
 		});
 		fail = true;
+	}else {
+		checkEmail();
 	}
 
 	if (document.getElementById('contractForm:amount').value.length == 0) {
@@ -28,7 +52,7 @@ function validateContract() {
 			message : "Payment cannot be null."
 		});
 		fail = true;
-	}
+	} 
 	if (document.getElementById('contractForm:contractContent').value.length == 0) {
 		$.growl.error({
 			message : "Content of the contract cannot be null."
@@ -40,9 +64,7 @@ function validateContract() {
 			message : "Validation date cannot be null."
 		});
 		fail = true;
-	} else {
-		checkEmail();
-	}
+	} 
 }
 
 function finishContract() {
@@ -61,6 +83,7 @@ function clearModal() {
 		$('#div_contract-to').find("input").val('').end();
 		$('#div_amount').find("input").val('').end();
 		$('#div_valid').find("input").val('').end();
+		$('#div_contractContent').find("input").val('').end();
 	});
 
 }
